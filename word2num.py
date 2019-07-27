cb433a9c1d8620d013183905c5f53f21d0927027
aeksei <aeksei94@gmail.com>
2019-07-27 14:07:30 +0300

    fix get_num function work with numbers 1000 <= n <= 999999

--- word2num.py
+++ word2num.py
@@ -13,11 +13,11 @@ def get_num(word_num):
         if word in rank:
             if (global_level is not None) and (global_level < nums[word][1]):
                 raise ValueError('global_level < {}'.format(nums[word][1]))
-            global_level = nums[word][1]
+            global_level = rank[word][1]
             if local_num == 0:
-                global_num += nums[word][0]
+                global_num += rank[word][0]
             else:
-                global_num += local_num * nums[word][0]
+                global_num += local_num * rank[word][0]
             local_num = 0
             local_level = None
 
@@ -44,10 +44,13 @@ def test_1000_999999():
     with open('numbers_1000_999999.txt', 'r') as f:
         for line in f:
             word_num = line[:-1].split(',')
-            num = get_num(word_num[1].split(' '))
+            num = get_num(parser.get_lemma(word_num[1]))
             if int(word_num[0]) != num:
                 raise ValueError('{} != {}'.format(num, word_num[1]))
+            else:
+                print('{} = {}'.format(num, word_num[1]))
 
 
 if __name__ == '__main__':
-    test_999()
+    # test_999()
+    # test_1000_999999()
